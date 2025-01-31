a40df1939a8cb777f3cc09e70d17549afbc77521
yello-lab <yellolabgh@gmail.com>
2025-01-31 23:49:37 +0000

    Add files via upload

--- assets/my.js
+++ assets/my.js
@@ -15,14 +15,14 @@ document.addEventListener("DOMContentLoaded", function () {
             const productPrice = productItem.querySelector(".new-price").getAttribute("data-price");
             const productImage = productItem.querySelector("img").getAttribute("data-image");
 
-            // Construct the WhatsApp message
+            // Construct the WhatsApp message with the image URL as a clickable link
             const message = `Hello, I would like to order the following product:\n\n*Product Name:* ${productName}\n*Price:* ₵${productPrice}\n*Image:* https://github.com/yello-lab/lawgeescloset/tree/main/${productImage}`;
 
             // Encode the message for the URL
             const encodedMessage = encodeURIComponent(message);
 
             // Open WhatsApp with the pre-filled message
-            const whatsappUrl = `https://wa.me/+233247770480?text=${encodedMessage}`;
+            const whatsappUrl = `https://wa.me/+233549031201?text=${encodedMessage}`;
             window.open(whatsappUrl, "_blank");
         });
     });
